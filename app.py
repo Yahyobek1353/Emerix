767ef77e7cbd841ff269b6c9e284345e8a6d4c40
SALIEV <147917630+Yahyobek1353@users.noreply.github.com>
2025-01-16 22:02:46 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask, jsonify, request
+from translations import get_translation
+
+app = Flask(__name__)
+
+@app.route('/translations')
+def translations():
+    lang = request.args.get('lang', 'en')  # Получаем язык из параметра запроса
+    translation = get_translation(lang)  # Получаем переводы для выбранного языка
+    return jsonify(translation)  # Отправляем переводы в формате JSON
+
+if __name__ == '__main__':
+    app.run(debug=True)
